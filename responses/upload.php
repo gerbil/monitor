099b5f7b5dedfc6c88448c5726d96f9f3c557fbe
edualapi <jandskip@gmail.com>
2015-02-16 16:21:21 +0200

    +4 new servers

--- responses/upload.php
+++ responses/upload.php
@@ -41,11 +41,21 @@ if (isset($_FILES['file'])) {
         $servers['hgdb2']['password'] = 'provdbpass042';
         $servers['hgdb2']['url'] = 'dbp-1.tele2.net/provvos7';
 
-        $servers['hgdb2']['user'] = 'provhgd02';
-        $servers['hgdb2']['password'] = 'provdbpass042';
-        $servers['hgdb2']['url'] = 'obelix.corp.tele2.com/PROVDB';
+        $servers['kstb2']['user'] = 'provkst02';
+        $servers['kstb2']['password'] = 'e2uPxreNbchUO80J';
+        $servers['kstb2']['url'] = 'obelix.corp.tele2.com/PROVDB';
 
-        /*WHAEVER sdadasdas  */
+        $servers['kstb3']['user'] = 'provkst03';
+        $servers['kstb3']['password'] = 'x7du8shQcMbFpmm1';
+        $servers['kstb3']['url'] = 'obelix.corp.tele2.com/PROVDB';
+
+        $servers['hgd-prov-b2']['user'] = 'provhgd02';
+        $servers['hgd-prov-b2']['password'] = 'fQVEQLPjy8InVexu';
+        $servers['hgd-prov-b2']['url'] = 'obelix.corp.tele2.com/PROVDB';
+
+        $servers['hgd-prov-b3']['user'] = 'provhgd03';
+        $servers['hgd-prov-b3']['password'] = 'RFMkcPynUUktLGcv';
+        $servers['hgd-prov-b3']['url'] = 'obelix.corp.tele2.com/PROVDB';
 
         foreach ($servers as $key => $value) {
 
